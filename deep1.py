10093b205fcc183e8374ce0f93ee49bbf6a5e22b
shima <shima.jin@icloud.com>
2017-02-13 13:14:49 +0900

    2.3.2 Perceptron 重みとバイアスの実装

--- deep1.py
+++ deep1.py
@@ -1,4 +1,6 @@
-def AND(x1, x2):
+import numpy as np
+
+def AND1(x1, x2):
 	w1, w2, theta = 0.5, 0.5, 0.7
 	tmp = x1*w1 + x2*w2
 	if tmp <= theta:
@@ -6,8 +8,22 @@ def AND(x1, x2):
 	elif tmp > theta:
 		return 1
 
+print(AND1(0,0))
+print(AND1(1,0))
+print(AND1(0,1))
+print(AND1(1,1))
 
-print(AND(0,0))
-print(AND(1,0))
-print(AND(0,1))
-print(AND(1,1))
+def AND2(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.7
+	tmp = np.sum(x*w) + b
+	if tmp <= 0:
+		return 0
+	else:
+		return 1
+
+print(AND2(0,0))
+print(AND2(1,0))
+print(AND2(0,1))
+print(AND2(1,1))

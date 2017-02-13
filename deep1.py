9b88bd019cc32a2847b09171922cb2c414856ecb
shima <shima.jin@icloud.com>
2017-02-13 15:36:05 +0900

    2.5.2 多層パーセプトロンでXORの実装

--- deep1.py
+++ deep1.py
@@ -61,3 +61,17 @@ print(OR(0,1))
 print(OR(1,0))
 print(OR(1,1))
 
+def XOR(x1, x2):
+	s1 = NAND(x1, x2)
+	s2 = OR(x1, x2)
+	y = AND1(s1, s2)
+	return y
+
+print("")
+print(XOR(0,0))
+print(XOR(0,1))
+print(XOR(1,0))
+print(XOR(1,1))
+
+
+

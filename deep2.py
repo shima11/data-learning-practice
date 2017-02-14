f790c5fbe6e086d845398f3f35ae0b68cd14622f
shima <shima.jin@icloud.com>
2017-02-14 23:12:02 +0900

    3.2.7 活性化関数（Relu関数）

--- deep2.py
+++ deep2.py
@@ -7,14 +7,26 @@ def step_function(x):
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+def relu(x):
+	return np.maximum(0,x)
 
+# step function
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = step_function(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+# sigmoid
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = sigmoid(x)
+# plt.plot(x,y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+# relu
 x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+y = relu(x)
 plt.plot(x,y)
-plt.ylim(-0.1, 1.1)
+plt.ylim(-0.1, 5.1)
 plt.show()
